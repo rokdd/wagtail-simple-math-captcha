90bb0e99aa7ea8addfcca7bdabea21e6a73db247
Jordan Markov <jmarkov@musat-bg.com>
2015-08-25 09:24:16 +0300

    Change README name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='wagtail-simple-math-captcha',
     version='0.1.0',
-    packages=['wagtail-simple-math-captcha'],
+    packages=['wagtailsimplemathcaptcha'],
     install_requires=['django-simple-math-captcha'],
     include_package_data=True,
     license='BSD License',
