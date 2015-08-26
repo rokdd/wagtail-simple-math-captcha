c696f5ae9d3a5badeb81d9ce7b1cee722bec7365
Jordan Markov <jmarkov@musat-bg.com>
2015-08-26 16:09:28 +0300

    Fix PyPI readme format and add download link

---
+++ register.py
@@ -0,0 +1,12 @@
+import pandoc
+import os
+
+pandoc.core.PANDOC_PATH = r'C:\Program Files (x86)\Pandoc\pandoc.exe'
+
+doc = pandoc.Document()
+doc.markdown = open('README.md').read()
+f = open('README.txt','w+')
+f.write(doc.rst)
+f.close()
+os.system("setup.py register -r pypitest")
+os.remove('README.txt')
--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
+if os.path.exists('README.txt'):
+    README = open('README.txt').read()
+
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -17,6 +20,7 @@ setup(
     description='A simple math captcha field for Wagtail Form Pages based on Django Simple Math Captcha.',
     long_description=README,
     url='https://bitbucket.org/jordanmarkov/wagtail-simple-math-captcha',
+    download_url='https://bitbucket.org/jordanmarkov/wagtail-simple-math-captcha/get/0.1.1.tar.gz',
     author='Jordan Markov',
     author_email='jmarkov@gmail.com',
     classifiers=[
