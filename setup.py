aafd46126e3580a26a7a8ceca94c2d7ea50b472e
Jordan Markov <jmarkov@musat-bg.com>
2015-08-30 17:35:15 +0300

    Add keywords to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
@@ -13,14 +13,15 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='wagtail-simple-math-captcha',
     version='0.1.2',
-    packages=['wagtailsimplemathcaptcha'],
-    install_requires=['django-simple-math-captcha'],
+    packages=find_packages(exclude=[]),
+    install_requires=['wagtail', 'django-simple-math-captcha'],
     include_package_data=True,
     license='BSD License',
     description='A simple math captcha field for Wagtail Form Pages based on Django Simple Math Captcha.',
     long_description=README,
     url='https://bitbucket.org/jordanmarkov/wagtail-simple-math-captcha',
     download_url='https://bitbucket.org/jordanmarkov/wagtail-simple-math-captcha/get/0.1.2.tar.gz',
+    keywords=['django', 'wagtail', 'cms', 'captcha'],
     author='Jordan Markov',
     author_email='jmarkov@gmail.com',
     classifiers=[
